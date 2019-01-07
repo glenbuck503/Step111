3afd0907805c06a41bca26c7cfebf33e6523cb4f
Glen Buck <glenbuck503@gmail.com>
2019-01-07 13:33:34 -0800

    add step 111

---
+++ Step111/Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step111
+{
+    public class Numbers
+    {
+        public static int Addition(int firstNumber)
+        {
+            int resultNumber = firstNumber + 10;
+            return resultNumber;
+        }
+
+        public static int Subtraction(int secondNumber)
+        {
+            int resultNumber2 = secondNumber - 5;
+            return resultNumber2;
+
+        }
+
+        public static int Multiply(int thirdNumber)
+        {
+            int resultNumber3 = thirdNumber * 2;
+            return resultNumber3;
+        }
+
+    }
+}
---
+++ Step111/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("What number would you like to add 10 to?");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int additionResult = Numbers.Addition(firstNumber);
+            Console.WriteLine(additionResult);
+          
+
+            Console.WriteLine("What number would you like to subtract 5 from?");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int subtractionResult = Numbers.Subtraction(secondNumber);
+            Console.WriteLine(subtractionResult);
+           
+
+            Console.WriteLine("What number would you like to multiply by 2?");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            int multiplyResult = Numbers.Multiply(thirdNumber);
+            Console.WriteLine(multiplyResult);
+            Console.ReadLine();
+
+        }
+    }
+}
